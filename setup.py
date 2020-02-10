0c5d6240a9fca2a567f803e6d1ad7f4ea8ac0ea0
StVladislav <sv6382@gmail.com>
2020-02-11 01:36:22 +0300

    Added drawdowns yield in maxdrawdown function.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
      name='statlab',
-     version='0.1',
+     version='0.2',
      scripts=['statlab_doc'] ,
      author="StVladislav",
      author_email="sv6382@gmail.com",
