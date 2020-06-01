302cc42e7f1c781e1aa71c1adef51ef787d1ea3e
StVladislav <sv6382@gmail.com>
2020-06-02 01:44:05 +0300

    some changes in logger.py

--- statlab/logger.py
+++ statlab/logger.py
@@ -1,7 +1,13 @@
 import logging
 
 
-def create_logger(name, level=logging.DEBUG, drop: bool = False):
+def create_logger(
+        name,
+        level_console=logging.DEBUG,
+        drop: bool = False,
+        path_to_save: str = None,
+        level_for_file=logging.WARNING
+):
     if name in logging.Logger.manager.loggerDict.keys():
         if drop:
             logging.Logger.manager.loggerDict.pop(name)
@@ -9,11 +15,20 @@ def create_logger(name, level=logging.DEBUG, drop: bool = False):
             return logging.getLogger(name)
 
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    logger.setLevel(level_console)
+
     ch = logging.StreamHandler()
-    ch.setLevel(level)
+    ch.setLevel(level_console)
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
+
     logger.addHandler(ch)
+
+    if path_to_save is not None:
+        fh = logging.FileHandler(path_to_save)
+        fh.setLevel(level_for_file)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
 
     return logger
